3f0cb9cb6207aed0f582b4cc678470bce03b4960
pgrimsehl <grimsehl.peter@gmail.com>
2017-04-28 18:40:12 +0200

    experimental: sort (broken)

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -14,8 +14,11 @@
 
 class A
 {
-public :
-	int myMethod() { return 0; };
+public:
+	int myMethod()
+	{
+		return 0;
+	};
 	u8 myValue = 0;
 };
 
@@ -26,7 +29,10 @@ class B
 class SubA : public A
 {
 public:
-	int myMethod() { return 1; };
+	int myMethod()
+	{
+		return 1;
+	};
 	std::string myValue;
 };
 
@@ -83,63 +89,126 @@ typelist5 list5;
 using typelist6 = mp::tl::push_back_t<typelist0, A, B, SubA, SubB>;
 typelist6 list6;
 
-using type0 = mp::tl::type_at_t<typelist0, 2>;
-type0 t0;
-using type1 = mp::fn::transform_t<typelist6, std::tuple>;
-type1 t1;
-using type2 = mp::fn::rename_t<typelist_pair, std::pair>;
-type2 t2;
-using type3 = mp::fn::rename_t<typelist6, std::tuple>;
-type3 t3;
-using type4 = std::conditional_t<mp::tl::contains_v<typelist0, int>, A, B>;
-type4 t4;
+//using type0 = mp::tl::type_at_t<typelist0, 2>;
+//type0 t0;
+//using type1 = mp::fn::transform_t<typelist6, std::tuple>;
+//type1 t1;
+//using type2 = mp::fn::rename_t<typelist_pair, std::pair>;
+//type2 t2;
+//using type3 = mp::fn::rename_t<typelist6, std::tuple>;
+//type3 t3;
+//using type4 = std::conditional_t<mp::tl::contains_v<typelist0, int>, A, B>;
+//type4 t4;
 
-class MyThing : public mp::gen::thing<
-	mp::gen::prop<"Name"_crc32, std::string>,
-	mp::gen::prop<"PosX"_crc32, f32>, 
-	mp::gen::prop<"PosY"_crc32, f32>, 
-	mp::gen::prop<"Width"_crc32, u16>,
-	mp::gen::prop<"Height"_crc32, u32>>
+class MyThing : public mp::gen::thing<mp::gen::prop<"Name"_crc32, std::string>, mp::gen::prop<"PosX"_crc32, f32>, mp::gen::prop<"PosY"_crc32, f32>,
+									  mp::gen::prop<"Width"_crc32, u16>, mp::gen::prop<"Height"_crc32, u32>>
 {
 public:
 	MyThing() = default;
 };
 
-mp::tl::front_t<typelist0> front_type;
-mp::tl::back_t<typelist0> back_type;
-mp::tl::pop_front_t<typelist0> pop_front_type;
-mp::tl::pop_back_t<typelist0> pop_back_type;
-mp::tl::inverse_t<typelist6> typelist6_inverse;
-mp::tl::replace_at_t<typelist6, 5, MyThing> typelist6_replaced5;
-mp::tl::replace_t<typelist2, char, MyThing> typelist2_replaced;
-mp::tl::replace_t<decltype(typelist2_replaced), short, i64> typelist2_replaced_replaced;
+mp::tl::front_t<typelist0>									  front_type;
+mp::tl::back_t<typelist0>									  back_type;
+mp::tl::pop_front_t<typelist0>								  pop_front_type;
+mp::tl::pop_back_t<typelist0>								  pop_back_type;
+mp::tl::inverse_t<typelist6>								  typelist6_inverse;
+mp::tl::replace_at_t<typelist6, 5, MyThing>					  typelist6_replaced5;
+mp::tl::replace_t<typelist2, char, MyThing>					  typelist2_replaced;
+mp::tl::replace_t<decltype( typelist2_replaced ), short, i64> typelist2_replaced_replaced;
 using erased_type = mp::tl::type_at_t<typelist2, 2>;
-mp::tl::erase_at_t<typelist2, 2> typelist2_erased_2;
-mp::tl::insert_at_t<decltype(typelist2_erased_2), 2, erased_type> typelist2_restored;
-mp::tl::swap_t<typelist2, 2, 3> typelist2_swap;
+mp::tl::erase_at_t<typelist2, 2>									typelist2_erased_2;
+mp::tl::insert_at_t<decltype( typelist2_erased_2 ), 2, erased_type> typelist2_restored;
+mp::tl::swap_t<typelist2, 2, 3>										typelist2_swap;
 
-template <u32 ID>
-u32 get_id()
+namespace sort_func
 {
-	return ID;
+	template <class T, class U>
+	struct select
+	{
+		using type = typename std::conditional<(sizeof(T) > sizeof(U)), T, U>::type;
+	};
+};
+
+namespace mp
+{
+	namespace tl
+	{
+		template <class L> struct sort;
+		template <template <class...> class L> struct sort<L<>>
+		{
+			using type = L<>;
+		};
+
+		// template <template <class...> class L, class... Ts> struct sort<L<Ts...>>
+		//{
+		//	using front_type  = front_t<L<Ts...>>;
+		//	using search_list = pop_front_t<L<Ts...>>;
+
+		//	template <class L, class T> struct find_smallest;
+		//	template <template <class...> class L, class T> struct find_smallest<L<>, T>
+		//	{
+		//		using type = T;
+		//	};
+		//	template <template <class...> class L, class... Ts, class U, class T> struct find_smallest<L<U, Ts...>, T>
+		//	{
+		//		using smaller_type = std::conditional_t<( sizeof( T ) > sizeof( U ) ), T, U>;
+		//		using recurse_list = find_smallest<L<Ts...>, smaller_type>;
+
+		//	public:
+		//		using type = typename recurse_list::type;
+		//	};
+
+		//	using type = typename find_smallest<search_list, front_type>::type;
+
+		//	// template <class L, class T> struct find_smallest_t = find_smallest<L,T>::type;
+		//};
+
+		template <template <class...> class L, class... Ts> struct sort<L<Ts...>>
+		{
+			template <class M, size_t I, size_t J> struct apply_order;
+			template <template <class...> class M, class... Us, size_t I> struct apply_order<M<Us...>, I, I>
+			{
+				using type = M<Us...>;
+			};
+			template <template <class...> class M, class... Us, size_t I, size_t J> struct apply_order<M<Us...>, I, J>
+			{
+				using type_at_i = type_at_t<M<Us...>, I>;
+				using type_at_j = type_at_t<M<Us...>, J>;
+
+				using selected_type = typename sort_func::select<type_at_i, type_at_j>::type;
+
+			public:
+				using type = typename std::conditional<std::is_same<selected_type, type_at_i>::value, 
+					typename apply_order<M<Us...>, I, J - 1>::type, 
+					typename apply_order<swap_t<M<Us...>, I, J>, I, J - 1>::type>::type;
+			};
+
+			using type = typename apply_order<L<Ts...>, 0, sizeof...(Ts) - static_cast<size_t>(1)>::type;
+
+			// template <class L, class T> struct find_smallest_t = find_smallest<L,T>::type;
+		};
+	}
 }
+
+using selector = sort_func::select<int, int>;
+
+mp::tl::sort<typelist2>::type sorted;
 
 int main()
 {
 	u32 udl0 = "CRC32 UDL"_crc32;
-
 
 	SubA suba;
 
 	suba.myMethod();
 	suba.A::myMethod();
 	std::string val_suba = suba.myValue;
-	u8 val_a = suba.A::myValue;
+	u8			val_a	= suba.A::myValue;
 
 	MyThing thingy;
 
-	f32 x = thingy.get<"PosX"_crc32>();
-	u32 height = thingy.get<"Height"_crc32>();
+	f32 x					   = thingy.get<"PosX"_crc32>();
+	u32 height				   = thingy.get<"Height"_crc32>();
 	thingy.get<"Name"_crc32>() = "TheThing";
 
 	bool a = mp::fn::is_baseclass_of<A, B>::value;
