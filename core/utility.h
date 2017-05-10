7db6991faefefa80900a7e194cf55b481f0e4240
pgrimsehl <grimsehl.peter@gmail.com>
2017-05-10 12:45:15 +0200

    added: compiler support for in_place_type (MSVC 2015)

--- core/utility.h
+++ core/utility.h
@@ -10,6 +10,23 @@ namespace core
 	};
 	constexpr in_place_t in_place{};
 
+#ifdef CORE_ONLY_STATIC_DATA_MEMBER_TEMPLATES_ALLOWED
+	template <class T> struct in_place_type_t
+	{
+		explicit constexpr in_place_type_t()
+		{
+		}
+	};
+	template <class T> constexpr in_place_type_t<T> in_place_type = in_place_type_t<T>{};
+
+	template <size_t I> struct in_place_index_t
+	{
+		explicit constexpr in_place_index_t()
+		{
+		};
+	};
+	template <size_t I> constexpr in_place_index_t<I> in_place_index = in_place_index_t<I>{};
+#else
 	template <class T> struct in_place_type_t
 	{
 		explicit in_place_type_t() = default;
@@ -21,4 +38,5 @@ namespace core
 		explicit in_place_index_t() = default;
 	};
 	template <size_t I> constexpr in_place_index_t<I> in_place_index{};
+#endif
 }
